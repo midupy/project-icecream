66d426d3909930726b1dba1edf35e5bd50d837c8
midupy <rodrigo.souza.pereira@gmail.com>
2023-06-27 10:56:09 -0300

    Update Frame.jsx

--- src/pages/Frame.jsx
+++ src/pages/Frame.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 // Libraries
-import styled from 'styled-components';
+import styled from "styled-components";
 
 // Components
 import Canvas from "../components/Canvas";
-import UploadImage from '../components/UploadImage';
-import Controller from '../components/Controller';
-import Download from '../components//Download';
+import UploadImage from "../components/UploadImage";
+import Controller from "../components/Controller";
+import Download from "../components//Download";
 
 const GridContainer = styled.div`
   width: 60%;
@@ -17,21 +17,21 @@ const GridContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`
+  flex-direction: column;
+`;
 
 const Column1 = styled.div`
-  width: 50%;
   margin-right: 1rem;
   background: #333333;
   padding: 1.5rem 1rem;
   border-radius: 7px;
-`
+`;
 
 const Column2 = styled.div`
   width: 50%;
   display: flex;
   flex-direction: column;
-`
+`;
 
 const Frames = () => {
   return (
@@ -42,7 +42,6 @@ const Frames = () => {
 
       <Column2>
         <UploadImage />
-        <Controller />
         <Download />
       </Column2>
     </GridContainer>
